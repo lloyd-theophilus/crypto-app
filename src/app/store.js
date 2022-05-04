aedced05dc8f898f77a330447375bf83582ae31e
Lloyd Theophilus <lloydtheophilus@gmail.com>
2022-05-04 23:31:49 +0100

    added a news article component to the home page and the news component

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 //Connecting the Api to the store
 import { cryptoApi } from "../services/cryptoApi";
+import { cryptoNewsApi } from "../services/cryptoNewsApi";
 
 export const store = configureStore({
     reducer: {
-        [cryptoApi.reducerPath]: cryptoApi.reducer
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer
     },
 })
 
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -25,24 +25,4 @@ export const cryptoApi = createApi({
 
 export const { useGetCryptosQuery } = cryptoApi;
 
-/*
-const axios = require("axios");
 
-const options = {
-  method: 'GET',
-  url: 'https://coinranking1.p.rapidapi.com/coins',
-  params: {
-    referenceCurrencyUuid: 'yhjMzLPhuIDl',
-    timePeriod: '24h',
-    'tiers[0]': '1',
-    orderBy: 'marketCap',
-    orderDirection: 'desc',
-    limit: '50',
-    offset: '0'
-  },
-  headers: {
-    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    'X-RapidAPI-Key': '8c69d91bafmsh744b7417d5fe210p1cda15jsn6e78c70d07f7'
-  }
-};
-*/
